f5f8cddc344017224dc3e19e77f23c7666360030
Sam Roman <samroman@Sams-MacBook-Pro.local>
2019-12-06 20:25:47 -0500

    Adds custom scrollview to profilevc

--- ingo-proto/Controllers/ProfileViewController.swift
+++ ingo-proto/Controllers/ProfileViewController.swift
@@ -12,24 +12,61 @@ class ProfileViewController: UIViewController {
 
     
     var user: AppUser!
-       var isCurrentUser = false
     
+    var isCurrentUser = false
+   
+    lazy var scrollView: UIScrollView = {
+        let sv = UIScrollView()
+        sv.isScrollEnabled = true
+        sv.backgroundColor = .white
+        sv.showsVerticalScrollIndicator = true
+        sv.decelerationRate = .fast
+        
+        return sv
+    }()
+    
+    
+    
+    
+    lazy var otherView: UIView = {
+        let ov = UIView()
+        ov.backgroundColor = .blue
+        return ov
+    }()
+    
+    
+    
+    
+    func constrainScrollView(){
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+       scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+       scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+       scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    }
+    
+    
+//    func constrainOtherView() {
+//        otherView.translatesAutoresizingMaskIntoConstraints = false
+//        otherView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+//        otherView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+//        otherView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor, constant: 0).isActive = true
+//        otherView.centerYAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor, constant: 900).isActive = true
+//    }
+    
+
     override func viewDidLoad() {
-        view.backgroundColor = .systemPurple
+        view.addSubview(scrollView)
+//        scrollView.addSubview(otherView)
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+        
+       }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        constrainScrollView()
+//        constrainOtherView()
+        self.scrollView.contentSize = CGSize(width: view.bounds.width, height: view.bounds.height + 500 )
     }
-    */
 
 }
